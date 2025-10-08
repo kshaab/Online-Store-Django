6744ae5a30e01df8f4fb312fb31fa8073a536561
Ксения <kssshaab@gmail.com>
2025-10-08 16:43:42 +0300

    base settings

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
 ]
